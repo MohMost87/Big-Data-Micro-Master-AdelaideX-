fa9f2eec964467ab9c8dd233264e3a12f17ce196
Mohamed Mostafa <M.M.Hassanein87@gmail.com>
2026-07-06 20:32:35 +0300

    sample project

---
+++ src/mypackage/Sample.java
@@ -0,0 +1,16 @@
+package mypackage;
+
+public class Sample {
+	
+	public static void main(String[] args){
+	    int sum = 0;
+
+	    //Calculate the sum from 1-10
+	    for(int i = 1; i < 10; i++){
+	        sum += i;
+	    }
+
+	    System.out.println(sum);
+	}
+
+}
